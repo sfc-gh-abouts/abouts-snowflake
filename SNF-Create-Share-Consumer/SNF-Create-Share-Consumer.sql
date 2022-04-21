7ba0038ed744c7be49960fbd9c7e7895c316d5c2
sfc-gh-abouts <97125220+abouts-snowflake@users.noreply.github.com>
2022-04-21 11:32:59 -0400

    Working complete data sharing prep
    Working complete data sharing prep

--- SNF-Create-Share-Consumer/SNF-Create-Share-Consumer.sql
+++ SNF-Create-Share-Consumer/SNF-Create-Share-Consumer.sql
@@ -24,16 +24,3 @@ grant all on warehouse andyb_demo_wh to role andyb_demo;
 
 -- the following grant is required to create a DB from a share
 grant create database on account to role andyb_demo;
-
-
--- the following should no longer be needed 
--- replaced the following with 
--- create or replace database andyb_demo_share;
--- grant usage on database andyb_demo_share to role andyb_demo;
-
--- create or replace schema andyb_demo_share.demo;
--- grant usage on schema andyb_demo_share.demo to role andyb_demo;
--- grant create table on schema andyb_demo_share.demo to role andyb_demo;
--- grant usage on all functions in database andyb_demo_share to role andyb_demo;
--- grant select on all tables in schema andyb_demo_share.demo to role andyb_demo;
--- grant select on future tables in database andyb_demo_share to role andyb_demo;
