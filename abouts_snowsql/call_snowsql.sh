17349534c7794f0ab3288381923e166b33ffb25d
sfc-gh-abouts <97125220+abouts-snowflake@users.noreply.github.com>
2022-07-11 16:39:21 -0400

    Update call_snowsql.sh
    Update

--- abouts_snowsql/call_snowsql.sh
+++ abouts_snowsql/call_snowsql.sh
@@ -2,6 +2,9 @@
 
 echo -e "\ncalling snowsql"
 echo -e "  using key pair auth"
-echo -e "  logging as john to citibike\n"
+echo -e "  logging as john to citibike"
+
+echo -e "\nor you could call to get jwt"
+echo -e "$ snowsql -a mva55597.us-east-1 -u john --private-key-path /Users/abouts/Documents/Key/snf_user_key.p8 --generate-jwt\n"
 
 snowsql -c abouts_demo
